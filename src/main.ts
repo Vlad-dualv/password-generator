f9e11965741853e37fd16165b8adece9672b0000
Vlad <vladswork95@gmail.com>
2025-05-12 16:32:00 +0100

    Typescript add

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,37 @@
-const passwordBox = document.querySelector('#password');
-const copyBtn = document.querySelector('.copy-icon');
-const generatePassBtn = document.querySelector('button');
+const passwordBox = document.querySelector<HTMLInputElement>('#password');
+const copyBtn = document.querySelector<HTMLImageElement>('.copy-icon');
+const generatePassBtn = document.querySelector<HTMLButtonElement>('button');
 
-const length = 12;
+const passwordLength = 12;
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
 const symbol = '[][]#~=+_-()*&^%$£!/?.>,<';
 const allChars = upperCase + lowerCase + number + symbol;
 
-function createPassword() {
+function createPassword(): void {
+  if (!passwordBox) {
+    return;
+  }
+
   let password = '';
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
   password += number[Math.floor(Math.random() * number.length)];
   password += symbol[Math.floor(Math.random() * symbol.length)];
 
-  while (length > password.length) {
+  while (password.length < passwordLength) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
   passwordBox.value = password;
 }
 
-function copyPassword() {
+function copyPassword(): void {
+  if (!passwordBox) {
+    return;
+  }
   navigator.clipboard.writeText(passwordBox.value);
 }
 
-generatePassBtn.addEventListener('click', createPassword);
-copyBtn.addEventListener('click', copyPassword);
+generatePassBtn?.addEventListener('click', createPassword);
+copyBtn?.addEventListener('click', copyPassword);
